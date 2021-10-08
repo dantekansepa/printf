ba33692c75c85919614eaa3e89c458207d5a3e67
Nyaradzo <88329176+Nyaradzo20@users.noreply.github.com>
2021-10-09 00:07:34 +0200

    betty

--- get_printer.c
+++ get_printer.c
@@ -9,65 +9,65 @@
 char *(*get_printer(char id))(va_list, printing_format *)
 {
 
-	char *(*printer)(va_list, printing_format *);
+char *(*printer)(va_list, printing_format *);
 
-	switch (id)
-	{
-		case 'c':
-			printer = _putchar;
-			break;
+switch (id)
+{
+case 'c':
+printer = _putchar;
+break;
 
-		case 's':
-			printer = _putstr;
-			break;
+case 's':
+printer = _putstr;
+break;
 
-		case 'd':
-			printer = _putint;
-			break;
+case 'd':
+printer = _putint;
+break;
 
-		case 'i':
-			printer = _putint;
-			break;
+case 'i':
+printer = _putint;
+break;
 
-		case 'u':
-			printer = _putuint;
-			break;
+case 'u':
+printer = _putuint;
+break;
 
-		case 'b':
-			printer = _putbin;
-			break;
-		case 'o':
-			printer = _putoct;
-			break;
+case 'b':
+printer = _putbin;
+break;
+case 'o':
+printer = _putoct;
+break;
 
-		case 'x':
-			printer = _puthex;
-			break;
+case 'x':
+printer = _puthex;
+break;
 
-		case 'X':
-			printer = _putHex;
-			break;
+case 'X':
+printer = _putHex;
+break;
 
-		case 'p':
-			printer = _putadress;
-			break;
+case 'p':
+printer = _putadress;
+break;
 
-		case 'r':
-			printer = _putrts;
-			break;
+case 'r':
+printer = _putrts;
+break;
 
-		case 'R':
-			printer = _putrot13;
-			break;
+case 'R':
+printer = _putrot13;
+break;
 
-		case 'S':
-			printer = _putS;
-			break;
+case 'S':
+printer = _putS;
+break;
 
-		default:
-			printer = NULL;
-			break;
-	}
+default:
+printer = NULL;
+break;
+}
 
-	return (printer);
+return (printer);
 }
